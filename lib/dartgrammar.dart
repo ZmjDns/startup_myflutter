3a13440c3002ce7cd297a6e6f8a7e46b261f13bc
ZmjDns <292570927@qq.com>
2019-03-27 21:04:14 +0800

    dart基础语法

--- lib/dartgrammar.dart
+++ lib/dartgrammar.dart
@@ -20,8 +20,43 @@ class aaa{
 // 这意味着函数可以赋值给变量或作为参数传递给其他函数，这是函数式编程的典型特征。
 
   bool isNoble(int atomicNumber){
-    //return _nobleGases[atomicNumber] != null;
+    return false;
   }
+
+
+  void _nobleGass(CALLLBACK ){
+
+  }
+
+  //函数作为参数传递
+void execute(var callback){
+    callback();
+}
+
+void test(){
+  execute(() => print("XXX"));
+}
+
+  //可选位置参数
+  String say(String from,String msg, [String device]){
+    var result = "$from says $msg";
+    if(device != null){
+      result = "$result with $device";
+    }
+    return result;
+  }
+
+  //可选的命名参数
+  //设置【bold】和【hidden】标志
+  void enableFlag({bool bold,bool hidden}){
+    //...
+  }
+  //调用函数时，可以使用指定命名参数
+void test2(){
+    enableFlag(bold: true,hidden: false);
+}
+
+
 
 
 }
